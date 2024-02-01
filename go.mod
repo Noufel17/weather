de935607cfeb3a204b4a616eb792b562ae99eb2a
noufel.naili <noufel.naili@namla.cloud>
2024-02-01 13:04:42 +0100

    first version

---
+++ go.mod
@@ -0,0 +1,12 @@
+module workshop/weather
+
+go 1.21.1
+
+require (
+	github.com/dolmen-go/kittyimg v0.0.0-20220904140504-22f6493b700f // indirect
+	github.com/enescakir/emoji v1.0.0 // indirect
+	github.com/fatih/color v1.16.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	golang.org/x/sys v0.14.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/enescakir/emoji"
+	"github.com/fatih/color"
+)
+
+type Weather struct {
+	Location struct {
+		Name string `json:"name"`
+		Country string `json:"country"`
+	}`json:"location"`
+	Current struct {
+		TempC float64 `json:"temp_c"`
+		Condition struct {
+			Text string `json:"text"`
+			Icon string `json:"icon"`
+		}`json:"condition"`
+	}`json:"current"` 
+	Forecast struct {
+		Forecastday []struct {
+			Hour []struct {
+				TimeEpoch int64 `json:"time_epoch"`
+				TempC float64 `json:"temp_c"`
+				Condition struct {
+					Text string `json:"text"`
+					Icon string `json:"icon"`
+				}
+				ChanceOfRain float64 `json:"chance_of_rain"`
+			}`json:"hour"`
+		}`json:"forecastday"`
+	}`json:"forecast"`
+
+}
+
+var emojis = map[string]emoji.Emoji{
+	"Clear":emoji.Sun,
+	"Sunny":emoji.Sun,
+	"Patchy rain":emoji.CloudWithRain,
+	"Partly cloudy":emoji.SunBehindCloud,
+	"Cloudy":emoji.Cloud,
+	"Rainy":emoji.CloudWithRain,
+}
+
+func IsSimpleCondition(category string) bool {
+    switch category {
+    case
+        "Sunny",
+		"Patchy rain",
+        "Partly cloudy",
+        "Cloudy",
+        "Rainy",
+		"Clear":
+        return true
+    }
+    return false
+}
+
+func main() {
+ res, err := http.Get("https://api.weatherapi.com/v1/forecast.json?key=94474d04349f43008d395834240102&q=Algiers&days=1&aqi=no&alerts=no")
+
+ if err != nil {
+  panic(err)
+ }
+
+ defer res.Body.Close()
+
+ if res.StatusCode != 200 {
+  panic("Weather API not working")
+ }
+
+ body, err := io.ReadAll(res.Body)
+
+ if err!= nil {
+  panic(err)
+ }
+ var weather Weather
+ err = json.Unmarshal(body,&weather)
+ if err != nil {
+	panic(err)
+ }
+ location, current, hours := weather.Location, weather.Current, weather.Forecast.Forecastday[0].Hour
+ var emj emoji.Emoji
+ currentCondition := strings.Trim(current.Condition.Text, " ") 
+ if IsSimpleCondition(currentCondition) {
+	emj = emojis[currentCondition]
+ }else {
+	emj = emoji.Cloud
+ }
+ fmt.Printf(
+	"%s, %s: %.0f C, %s %s\n",
+    location.Name,location.Country,
+	current.TempC,
+	currentCondition,
+	emj,
+ )
+
+ for _,hour := range hours {
+	date := time.Unix(hour.TimeEpoch,0)
+	// skip hours that are before the current hour
+	if date.Before(time.Now()){
+		continue
+	}
+	var emjhr emoji.Emoji
+	condition := strings.Trim(hour.Condition.Text, " ") 
+	if IsSimpleCondition(condition ) {
+		emjhr = emojis[condition]
+	}else {
+		emjhr = emoji.Cloud
+	}
+	message := fmt.Sprintf(
+		"%s - %.0f C°, %.0f%%, %s %s\n",
+		date.Local().Format("15:04"),
+		hour.TempC,
+		hour.ChanceOfRain,
+		condition,
+		emjhr,
+	)
+	
+	if(hour.ChanceOfRain >= 50){
+		color.Red(message)
+	}else{
+		fmt.Print(message)
+	}
+ }
+}
